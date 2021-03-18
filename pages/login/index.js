8668b6bd944656fcc09f4571714b46ce23d4459c
ela_lisondra <emrlisondra@gmail.com>
2021-03-18 15:32:23 +0800

    includes capstone 3 day 4 frontend

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,6 +1,7 @@
 import {useState, useEffect, useContext} from 'react'
 import {Form, Button,Container} from 'react-bootstrap'
 import Swal from 'sweetalert2'
+import {GoogleLogin} from 'react-google-login'
 
 //use this to redirect our user
 import Router from 'next/router'
@@ -116,6 +117,66 @@ export default function Login(){
 
 	}
 
+	function authenticateGoogleToken(response){
+        console.log(response)
+        fetch('http://localhost:8000/api/users/verify-google-id-token',{
+            method: 'POST',
+            headers: {
+                'Content-Type':'application/json'               
+            },
+            body: JSON.stringify({
+                tokenId: response.tokenId,
+                accessToken: response.accessToken
+            })
+        })
+        .then(res=>res.json())
+        .then(data=> {
+            if(typeof data.accessToken !== 'undefined'){
+                localStorage.setItem('token', data.accessToken)
+
+                fetch('http://localhost:8000/api/users/details',{
+                    headers: {
+                        'Authorization': `Bearer ${data.accessToken}`
+                    }
+                })
+                .then(res => res.json())
+                .then(data => {
+                    localStorage.setItem('email', data.email)
+                    localStorage.setItem('isAdmin', data.isAdmin)
+
+                    setUser({
+                        email:data.email,
+                        isAdmin:data.isAdmin
+                    })
+
+                    Swal.fire({
+                        icon:'success',
+                        title: 'Successful Login'
+                    })
+                    Router.push('/courses')
+                })
+            } else {
+                if(data.error === "google-auth-error"){
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Google Authentication Failed.'
+                    })
+                } else if(data.error === "login-type-error"){
+                    Swal.fire({
+                        icon:'error',
+                        title: 'Login Failed',
+                        text: 'You may have registered through a different procedure.'
+                    })
+                }
+            }
+            // console.log(data)
+        })
+    }
+
+    function failed(response){
+        console.log(response)
+    }
+
 	return(
 
 			<Container>
@@ -138,6 +199,15 @@ export default function Login(){
 						<Button variant="primary" type="submit" disabled className="btn-block">Submit</Button>
 
 				    }
+
+		             <GoogleLogin 
+		                clientId="417135322331-a6tv9eq9sf6cp410cts4jva4bvcvfc4s.apps.googleusercontent.com"
+		                buttonText="Login Using Google"
+		                onSuccess={authenticateGoogleToken}
+		                onFailure={authenticateGoogleToken}
+		                cookiePolicy={'single_host_origin'}
+		                className="w-100 my-40 text-center d-flex justify-content-center"            
+		            />
 		     </Form>
 		 </Container>
 
